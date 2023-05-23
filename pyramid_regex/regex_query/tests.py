58fbb44126b8e320e91d92f75e04b11809a4be9d
Ojima Grace Ohaba <ojimaisback@yahoo.com>
2023-05-23 21:32:02 +0100

    added tests

--- pyramid_regex/regex_query/tests.py
+++ pyramid_regex/regex_query/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class QueryToolTests(TestCase):
+    def test_home_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_full_match(self):
+        response = self.client.post(reverse('full_match'), {'text': 'Sample text', 'regex_query': 'sample'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'fullmatch.html')
+        
+    def test_first_match(self):
+        response = self.client.post(reverse('first_match'), {'text': 'Sample text', 'regex_query': 'sample'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'firstmatch.html')
+
