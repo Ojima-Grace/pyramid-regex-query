b5e93d1e8137d6ccbc6cbd5a317a463ffe3d8391
Ojima Grace Ohaba <ojimaisback@yahoo.com>
2023-05-23 22:09:06 +0100

    a little correction

--- pyramid_regex/pyramid_regex/urls.py
+++ pyramid_regex/pyramid_regex/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('regex_query.urls')),
+    path('', include('regex_querry.urls')),
 ]
